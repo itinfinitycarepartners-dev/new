48d3913c08b1f01b77d9effba75506563ded1a19
WKinja <it.infinitycarepartners@gmail.com>
2026-08-04 13:45:12 +0300

    Fixed documents

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -860,9 +860,6 @@ export default function Dashboard() {
             headers: {
               Authorization: `Bearer ${token}`,
               "Content-Type": "application/json",
-              "Cache-Control": "no-cache, no-store, must-revalidate",
-              Pragma: "no-cache",
-              Expires: "0",
             },
           }
         );
